1e0e2e24d21d74620a8d2d4afa1c589e396ab050
Catherine Omego <omegocatherine@gmail.com>
2022-10-18 18:26:40 -0700

    Team Project 0

--- main.h
+++ main.h
@@ -41,9 +41,6 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-
-int _printf(const char *format, ...);                                                                                                                  
-int handle_print(const char *fmt, int *i,
 va_list list,char buffer[], int flags, int width, int precision, int size);
 
 /************FUNCTIONS**********/
