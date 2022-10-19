a0efdddd81e6707e965f4ffa66e9154689e9bebd
christianah613 <christianah613@gmail.com>
2022-10-18 17:22:05 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -20,25 +20,24 @@ if (format[i] != '%')
 {
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
+print_buffer(buffer, &buff_ind)
 /* write(1, &format[i], 1);*/
 printed_chars++;
 }
 else
 {
-print_buffer(buffer, &buf_ind);
+print_buffer(buffer, &buff_ind);
 flags = get_flags(format, &i);
 width = get_width(format, &i, list);
 precision = get_precision(format, &i, list);
 size = get_size(format, &i);
 ++i;
-printed = handle_print(format, &i, list, buffer
-flags, width, precision, size);
-if (printed== -1)
+printed = handle_print(format, &i, list, buffer,flags, width, precision, size);
+if (printed == -1)
 return (-1);
-}
-}												
 printed_chars += printed;
+}
+}
 print_buffer(buffer, &buff_ind);
 va_end(list);
 return (printed_chars);
@@ -52,5 +51,5 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
-*buff_ind = 0
+*buff_ind = 0;
 }
